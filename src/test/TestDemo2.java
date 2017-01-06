5884a4723c8f97cef7566bbb5e5a72bf65bd405e
Wing9310 <745092402@qq.com>
2017-01-06 11:48:07 +0800

    add class

---
+++ src/test/TestDemo2.java
@@ -0,0 +1,11 @@
+package test;
+
+public class TestDemo2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println((int)(Math.random()*9));
+		System.out.println(Math.random());
+	}
+
+}
