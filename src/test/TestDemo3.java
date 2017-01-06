3cccaee9805872c98a782384083ee27b46b5d2b2
Wing139 <745092402@qq.com>
2017-01-06 14:32:26 +0800

    test return2

--- src/test/TestDemo3.java
+++ src/test/TestDemo3.java
@@ -1,10 +1,10 @@
 package test;
 
-public class TestDemo2 {
+public class TestDemo3 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println((int)(Math.random()*9));
+		System.out.printf("%d",12);
 	}
 
 }
